6c476a75cc8d3e02c7a24a162704e80350a7e0d5
samuel orji <awesomeorji@gmail.com>
2018-05-26 16:28:48 +0100

    added fragments

---
+++ src/main/java/com/example/samuel/firestore/issuesFragment.java
@@ -0,0 +1,29 @@
+package com.example.samuel.firestore;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class issuesFragment extends Fragment {
+
+
+    public issuesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_issues, container, false);
+    }
+
+}
