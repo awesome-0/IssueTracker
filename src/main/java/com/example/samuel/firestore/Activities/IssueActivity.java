75cbd9b23d348546dee4389c9ead6d1b7793fc32
samuel orji <awesomeorji@gmail.com>
2018-05-26 18:26:52 +0100

    added new project activity

--- src/main/java/com/example/samuel/firestore/Activities/IssueActivity.java
+++ src/main/java/com/example/samuel/firestore/Activities/IssueActivity.java
@@ -1,13 +1,32 @@
 package com.example.samuel.firestore.Activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.samuel.firestore.PagerAdapter;
+import com.example.samuel.firestore.R;
+
 public class IssueActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issue);
+
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab);
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setText("ISSUES");
+        tabLayout.getTabAt(1).setText("PROJECT");
+
+
+
     }
 }
---
+++ src/main/java/com/example/samuel/firestore/PagerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.samuel.firestore;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+       if(position == 0){
+           return new issuesFragment();
+       }else return new ProjectFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if(position == 0){
+            return "ISSUES";
+        }else return "PROJECT";
+    }
+}
