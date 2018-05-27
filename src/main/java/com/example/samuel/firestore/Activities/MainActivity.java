38f1534c08a6bac0d7975c86c9df4e459a90ac3e
samuel orji <awesomeorji@gmail.com>
2018-05-27 08:37:29 +0100

    added menu for main activity

--- src/main/java/com/example/samuel/firestore/Activities/MainActivity.java
+++ src/main/java/com/example/samuel/firestore/Activities/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.samuel.firestore.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.samuel.firestore.R;
 
@@ -12,6 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this,IssueActivity.class));
+       // startActivity(new Intent(this,IssueActivity.class));
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        toolbar.setTitle("Tracker");
+        setSupportActionBar(toolbar);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+       int id =  item.getItemId();
+       if(id == R.id.issuse_Activity){
+           startActivity(new Intent(this,IssueActivity.class));
+       }
+        return super.onOptionsItemSelected(item);
     }
 }
